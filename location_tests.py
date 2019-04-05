f5360e885bceee613e62d6f36c0ddb0039878160
Sam Burns <samburns157@gmail.com>
2019-04-04 17:19:31 -0700

    Added location test cases.

--- location_tests.py
+++ location_tests.py
@@ -6,8 +6,16 @@ class TestLab1(unittest.TestCase):
     def test_repr(self):
         loc = Location("SLO", 35.3, -120.7)
         self.assertEqual(repr(loc),"Location('SLO', 35.3, -120.7)")
-    
-    # Add more tests!
+
+    def test_eq(self):
+        loc1 = Location("SLO", 35.4, -120.7)
+        loc2 = Location("SLO", 35.4, -120.7)
+        self.assertEqual(loc1, loc2)
+
+    def test_not_eq(self):
+        loc1 = Location("SLO", 35.4, -120.7)
+        loc2 = Location("Null Island", 0, 0)
+        self.assertNotEqual(loc1, loc2)
 
 if __name__ == "__main__":
         unittest.main()
